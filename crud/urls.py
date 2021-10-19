03dd39d722810939d2f9dffc398ae6294364f392
vinnieoh <viniciusoliveira926@gmail.com>
2021-10-19 12:39:02 -0300

    Criacao config do create

--- crud/urls.py
+++ crud/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from .views import HomeView    
+from .views import HomeView, CreateProdutoView, UpdateProdutoView, DeleteProdutoView
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home')
+    path('', HomeView.as_view(), name='home'),
+    path('add-produto/', CreateProdutoView.as_view(), name='produto_create'),
+    path('<int:pk>/update-produto/', UpdateProdutoView.as_view(), name='produto_update'),
+    path('<int:pk>/delete-produto/', DeleteProdutoView.as_view(), name='produto_delete'),
 ]
