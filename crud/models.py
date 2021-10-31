a555b9e7a7c1a288416a4308b56005be0732ea83
vinnieoh <viniciusoliveira926@gmail.com>
2021-10-31 15:13:03 -0300

    models

--- crud/models.py
+++ crud/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Produto(models.Model):
     produto = models.CharField(max_length=100)
-    quantidade = models.IntegerField(blank=True)
+    quantidade = models.PositiveIntegerField(blank=True)
     preco = models.DecimalField(max_digits=8, decimal_places=2, blank=True)
 
 
